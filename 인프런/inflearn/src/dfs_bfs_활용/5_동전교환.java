b0221e989b7dd537a5702a77e81ace557aee7a20
Seunghee <80750839+lsh9295@users.noreply.github.com>
2023-10-06 19:50:20 +0900

    [인프런 DFS, BFS 활용] 5. 동전교환(BFS)

--- 인프런/inflearn/src/dfs_bfs_활용/5_동전교환.java
+++ 인프런/inflearn/src/dfs_bfs_활용/5_동전교환.java
@@ -1,3 +1,5 @@
+package dfs_bfs_활용;
+
 import java.util.*;
 import java.io.*;
 
@@ -7,26 +9,24 @@ public class Main {
     static int[] ch = new int[501];
     static int[] coins;
     static Queue<Integer> Q = new LinkedList<>();
+
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         coins = new int[N];
-
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         int i = 0;
+
         while(st.hasMoreTokens()) {
             coins[i] = Integer.parseInt(st.nextToken());
             i++;
         }
         M = Integer.parseInt(br.readLine());
-
+        Q.offer(0);
         System.out.println(BFS(1));
     }
 
     public static int BFS(int L) {
-        Q.offer(0);
-
         while(!Q.isEmpty()) {
             int len = Q.size();
 
@@ -35,11 +35,9 @@ public class Main {
 
                 for (int j = 0; j < N; j++) {
                     int nx = x + coins[j];
-                    System.out.println(nx);
 
                     if (ch[nx] == 0 && nx == M)
                         return L;
-
                     ch[nx] = 1;
                     Q.offer(nx);
                 }
