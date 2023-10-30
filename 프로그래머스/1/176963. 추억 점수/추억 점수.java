61a2c84cbbfe830815397c0ab02ca04c8d9789d7
Seunghee <80750839+lsh9295@users.noreply.github.com>
2023-10-30 17:47:16 +0900

    [level 1] Title: 추억 점수, Time: 0.04 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        
+        for (int i = 0; i < photo.length; i++) {
+            int count = 0;
+            
+            for (int j = 0; j < photo[i].length; j++) {
+                if (map.get(photo[i][j]) != null)
+                    count += map.get(photo[i][j]);
+            }
+            answer[i] = count;
+        }
+        
+        return answer;
+    }
+}
