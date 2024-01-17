0bc230e4aad3254feb7d1a8593e4fb5b61f65cc1
Seunghee <80750839+lsh9295@users.noreply.github.com>
2024-01-17 16:38:06 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Baekjoon 25083 새싹
+ */
+class Main {
+    public static void main(String[] args) throws Exception {
+        String s = "         ,r'\"7\n" + 	// \", \n 이 제어문자다.
+                "r`-_   ,'  ,/\n" + 		// \n 이 제어문자다.
+                " \\. \". L_r'\n" + 		// \\, \", \n 이 제어문자다.
+                "   `~\\/\n" + 			// \\, \n 이 제어문자다.
+                "      |\n" + 			// \n 이 제어문자다.
+                "      |";
+
+        System.out.print(s);
+    }
+}
